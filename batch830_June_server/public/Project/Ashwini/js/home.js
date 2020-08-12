bcfe8e9a7a680e71c05b38d3a68122e6479189d5
AshwiniKotla <67404123+AshwiniKotla@users.noreply.github.com>
2020-08-13 01:19:37 +0530

    Project
    home page

---
+++ batch830_June_server/public/Project/Ashwini/js/home.js
@@ -0,0 +1,10 @@
+function womenKurthas(){
+	var link = document.querySelector("#w_cate_1").href;
+	document.querySelector("#w_iframe").setAttribute("src", "link");
+	console.log("hi");
+}
+function menShirts(){
+	var link = document.querySelector("#m_cate_1").href;
+	document.querySelector("#w_iframe").setAttribute("src", "link");
+	console.log("hi");
+}
