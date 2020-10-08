f0540edfa423ce1ff55bc3600f83bf6622267973
mahapatra079 <64601489+mahapatra079@users.noreply.github.com>
2020-10-08 10:31:10 +0530

    adding assignment_11
    understanding on if else

--- Batch_7am_SEP_20/public/Assignments/AMIT/A11/js/prompt.js
+++ Batch_7am_SEP_20/public/Assignments/AMIT/A11/js/prompt.js
@@ -1,31 +1,46 @@
-//Dynamic Search
+//Dynamic typecasting
 
-var employeeName, employeeGender, employeeName, departmentName;
+var employeeName, employeeGender, employeeName, departmentName, basicSalary;
 
 // using prompt
 
 employeeName = prompt("Enter the Student Name");
 employeeGender = prompt("Mention your Gender here");
-
-//prompt variables declararing
-
-var m1 = prompt("Enter the m1 details");
-m1 = parseInt(m1) //converts string to number
-var m2 = prompt("Enter the m2 details");
-m2 = parseInt(m2)
-var m3 = prompt("Enter the m3 details");
-m3 = parseInt(m3)
-var m4 = prompt("Enter the m4 details");
-m4 = parseInt(m4)
-var m5 = prompt("Enter the m5 details");
-m5 = parseInt(m5)
+departmentName = prompt("Enter Department Name");
+basicSalary = prompt("Enter the basic salary of the employee");
 
 console.log("employeeName is " + employeeName);
 console.log("employeeGender is " + employeeGender);
+console.log("departmentName is " + departmentName);
+console.log("basicSalary is " + basicSalary);
+
+var pf = ((basicSalary * 12) / 100);
+console.log("pf is " + pf);
+var hra = ((basicSalary * 24) / 100);
+console.log("hra " + hra);
+var totalSalary = pf + hra + basicSalary;
+console.log("totalSalary is " + totalSalary);
+var taxPercent = 0;
+var male;
+var female;
 
 
-// total & avg
-var total = m1 + m2 + m3 + m4 + m5;
-console.log("total is" + total);
-var avg = total / 5;
-console.log("avg is " + avg);
+if (employeeGender === male) {
+    if (totalSalary > 30000) {
+        taxPercent = 15;
+
+    } else if (totalSalary > 60000) {
+        taxPercent = 10;
+    }
+}
+if (employeeGender === female) {
+    if (totalSalary > 30000) {
+        taxPercent = 15;
+
+    } else if (totalSalary > 60000) {
+        taxPercent = 25;
+    }
+}
+
+var taxValue = ((basicSalary * taxPercent) / 100);
+console.log("tax value balance " + taxValue);
