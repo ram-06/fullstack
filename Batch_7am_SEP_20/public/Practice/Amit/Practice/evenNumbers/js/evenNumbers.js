aff37cbb17da999404104ab4568729eae651f40e
mahapatra079 <64601489+mahapatra079@users.noreply.github.com>
2020-10-20 12:31:21 +0530

    assignment19_evenNumbers

--- Batch_7am_SEP_20/public/Practice/Amit/Practice/evenNumbers/js/evenNumbers.js
+++
@@ -1,9 +0,0 @@
-// Display Even Numbers(1-100)
-var evenNumbers;
-
-for (var i = 0; i <= 100; i++) {
-    if (i % 2 == 0) {
-        console.log("evenNumbers is " + i);
-
-    }
-}
--- Batch_7am_SEP_20/public/Practice/Amit/Practice/oddNumbers/js/odd.js
+++
@@ -1,9 +0,0 @@
-// Display Odd Numbers
-var oddNumbers;
-
-for (var i = 35; i <= 99; i++) {
-    if (i % 2 != 0) {
-        console.log("oddNumbers is " + i);
-
-    }
-}
