3e35fa6417ac40776be787abd93c7de42f8f218c
durgaprasadUI <62068712+durgaprasadUI@users.noreply.github.com>
2020-12-16 09:03:18 +0530

    Dec 16
    DEC 16

---
+++ Batch_7am_SEP_20/public/ECMA/DEC_16/math.js
@@ -0,0 +1,17 @@
+/*var util = {};
+
+util.sum = function(a, b) {
+	var result = 0;
+	result = a + b;
+	return result;
+}
+
+util.pi = 3.14; */
+
+export function sum(a, b) {
+	var result = 0;
+	result = a + b;
+	return result;
+}
+
+export var pi = 3.14;
---
+++ Batch_Nov_830_2020/public/Dec_16/data.js
@@ -0,0 +1,28 @@
+var contactDetails = {
+	"fname": 'raj',
+	"lname": 'Krish',
+	phone_no: {
+		officNo: 2345,
+		prsnl: [34234,2342,2342]
+	},
+	email: {
+		office: 'ofice@office.com',
+		prsnl: ["abc@gmail.com", "test@yahoo.com"]
+	},
+	address: {
+		office: {
+			cname: 'India',
+			pin: 23423,
+			location: 'Hyd'
+		},
+		prsnl: {
+			dno: '34/A',
+			country: 'USA',
+			pin: 234
+		}
+	},
+	socialId: {
+		twiterid: 'prasad!',
+		fbId: 'abc'
+	}
+};
