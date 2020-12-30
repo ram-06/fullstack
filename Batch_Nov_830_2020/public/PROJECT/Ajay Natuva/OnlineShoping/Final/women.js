6caaae39689f44f3ac0b834a67a9b06f7b30038d
ajaynatuva <ajaynatuva039@gmail.com>
2020-12-30 17:52:43 +0530

    AjayNatuva_FinalProjectDemo
    Final Project Demo

---
+++ Batch_Nov_830_2020/public/PROJECT/Ajay Natuva/OnlineShoping/Final/women.js
@@ -0,0 +1,17 @@
+				
+function details(){
+				<div style="margin-top:8px;margin-left:140px;font-weight:bold;"><a href="#Men'sClothes">Shirts</a></div><hr>
+				<div style="margin-top:10px;margin-left:140px;font-weight:bold;"><a href="#WomensClothes">T-shirts</a></div> <hr>
+				<div style="margin-top:10px;margin-left:140px;font-weight:bold;"><a href="#baby">Jackets</a></div> <hr>
+				<div style="margin-top:10px;margin-left:140px;font-weight:bold;"><a href="#men">Formal Shirts</a></div> <hr>
+				<div style="margin-top:10px;margin-left:140px;font-weight:bold;"><a href="#Womens">Causal Shirts</a></div> <hr>
+				<div style="margin-top:10px;margin-left:140px;font-weight:bold;"><a href="#Electronics">Bottom Wear</a></div> <hr>
+				<div style="margin-top:10px;margin-left:140px;font-weight:bold;"><a href="#Tv's">Jeans</a></div> <hr>
+				<div style="margin-top:10px;margin-left:140px;font-weight:bold;"><a href="#hime">Formal Towsers</a></div> <hr>
+				<div style="margin-top:10px;margin-left:140px;font-weight:bold;"><a href="#sports">Causal Towsers</a></div> <hr>
+				<div style="margin-top:10px;margin-left:140px;font-weight:bold;"> <a href="#books">Shorts</a></div> <hr>
+				<div style="margin-top:10px;margin-left:140px;font-weight:bold;"><a href="#Toys">Lungi</a></div><hr>
+				<div style="margin-top:10px;margin-left:140px;font-weight:bold;"><a href="#Mobiles">Cargos</a></div><hr>
+				<div style="margin-top:10px;margin-left:140px;font-weight:bold;bottom:10px;"><a href="#Cosmatics">Suits&Blazers</a></div>
+				</div>
+			}
