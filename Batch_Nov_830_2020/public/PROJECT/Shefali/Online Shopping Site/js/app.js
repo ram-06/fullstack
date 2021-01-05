3655728a3d9580917e1dcc2ecec68688f404189d
shefaliDalela <69523195+shefaliDalela@users.noreply.github.com>
2021-01-05 09:40:06 +0530

    project

--- Batch_Nov_830_2020/public/PROJECT/Shefali/Online Shopping Site/js/app.js
+++ Batch_Nov_830_2020/public/PROJECT/Shefali/Online Shopping Site/js/app.js
@@ -5,3 +5,7 @@ $(".textbox input").on("blur", function () {
     if ($(this).val() == "")
         $(this).removeClass("focus");
 });
+function signInForm(){
+    /* $(location).attr("href", "register.html") */
+    window.location.assign("register.html");
+}
