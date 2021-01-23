04f8bb6066d271033dc2958546a90f8ea6d0e572
unknown <webdev.prasad@gmail.com>
2021-01-23 09:31:04 +0530

    JAN 23
    JAN 23

--- Batch_Nov_830_2020/public/Jan_19/dateWorker.js
+++ Batch_Nov_830_2020/public/Jan_19/dateWorker.js
@@ -1,1 +1,13 @@
-console.log("am from worker...")
+function getDateAndTime() {
+	var date = new Date();
+	date = date + '';
+	date = date.replace(/GMT.*/, '');
+	console.log(document)
+	postMessage(date);
+
+}
+
+setInterval(function(){
+	getDateAndTime();
+}, 1000);
+
